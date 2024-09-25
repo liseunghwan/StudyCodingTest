032ffa4604e495cdea01d32a850b1f9b8a2f98c5
liseunghwan <flowhwan45@naver.com>
2024-09-25 15:13:24 +0900

    Move files to week1

---
+++ week1/no3.py
@@ -0,0 +1,5 @@
+#두 수의 곱하기
+
+def solution(num1, num2):
+    answer = num1*num2
+    return answer
---
+++ week1/test.py
@@ -0,0 +1,5 @@
+#두 수의 합구하기
+
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
